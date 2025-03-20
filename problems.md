c7989b76a05319032d7a74bf4a8b1b75f51929cb
MrigeshDeshpande <mrigeshdeshpande246+code@gmail.com>
2025-03-20 05:29:53 +0000

    Added Codeforces problem & solution for 2025-03-20

---
+++ problems.md
@@ -0,0 +1,3 @@
+## Problem for 2025-03-20
+**[Border](https://codeforces.com/contest/1010/problem/C)**
+
---
+++ solutions/solution_2025-03-20.py
@@ -0,0 +1,7 @@
+# Solution for 2025-03-20 (Border)
+def solve():
+    # TODO: Implement solution
+    print('Hello, Codeforces!')
+
+if __name__ == '__main__':
+    solve()
