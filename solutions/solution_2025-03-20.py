6e9e573bc001878d83102e411af6acacad84b1af
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-03-20 11:13:08 +0530

    Update solution_2025-03-20.py

--- solutions/solution_2025-03-20.py
+++ solutions/solution_2025-03-20.py
@@ -1,7 +1,22 @@
-# Solution for 2025-03-20 (Border)
-def solve():
-    # TODO: Implement solution
-    print('Hello, Codeforces!')
+from math import gcd
+from functools import reduce
 
-if __name__ == '__main__':
-    solve()
+# Function to calculate GCD of a list
+def gcd_list(numbers):
+    return reduce(gcd, numbers)
+
+# Input
+n, k = map(int, input().split())  # Number of denominations, base of the system
+a = list(map(int, input().split()))  # Denominations of banknotes
+
+# Calculate the GCD of all the denominations modulo k
+g = gcd_list([x % k for x in a])
+
+# Collect all valid values of d
+result = []
+for d in range(0, k, g):  # Possible values are multiples of GCD within range
+    result.append(d)
+
+# Output the results
+print(len(result))
+print(" ".join(map(str, result)))
